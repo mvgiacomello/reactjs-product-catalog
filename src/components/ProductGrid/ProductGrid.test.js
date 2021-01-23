3f91e4e6f53260961bc4b1d218f47330a52636a6
Mauricio Giacomello <mvgiacomello@gmail.com>
2021-01-23 19:17:32 +0000

    Fixing tests for ProductGrid

--- src/components/ProductGrid/ProductGrid.test.js
+++ src/components/ProductGrid/ProductGrid.test.js
@@ -5,39 +5,26 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { render, screen, waitFor } from '@testing-library/react';
 import ProductGrid from './ProductGrid';
 
+const ENDPOINT = process.env.ENDPOINT || 'https://demo4690370.mockable.io'
 const dummyResponse = [
     {
-        'id': 111,
-        'title': 'John Legend Live',
-        'description': 'Come and join one of the world\'s most beloved artists as he performs live and just for you from MelodyVR’s studio in LA.',
-        'artist': 'John Legend',
+        'id': 1,
+        'title': 'Dummy Title',
+        'description': 'Dummy Description',
+        'artist': 'Dummy Artist',
         'isFree': false,
         'image1': 'https://d17jafawxl91z1.cloudfront.net/MVR_FDC_L0103_App_Hero-1607341456001.jpg',
         'image2': 'https://d17jafawxl91z1.cloudfront.net/MVR_FDC_L0103_App_Square-1607341456001.jpg',
-        'accentColor': '#f52459',
-        'backgroundColor': '#841225',
-        'textColor': '#ffffff',
-        'feature_order': '4',
-        'price': '12.99'
-    },
-    {
-        'id': 114,
-        'title': 'Fontaines D.C. in London',
-        'description': 'Live show from Brixton Academy.',
-        'artist': 'Fontaines D.C.',
-        'isFree': false,
-        'image1': 'https://d17jafawxl91z1.cloudfront.net/MVR_JOL_L0022_App_Hero-1589529179871.jpg',
-        'image2': 'https://d17jafawxl91z1.cloudfront.net/MVR_JOL_L0022_App_Square-1589535273302.jpg',
-        'accentColor': '#8a8989',
-        'backgroundColor': '#424242',
-        'textColor': '#FFCCCC',
-        'feature_order': '3',
-        'price': '12.99'
+        'accentColor': '#000000',
+        'backgroundColor': '#000000',
+        'textColor': '#000000',
+        'feature_order': '1',
+        'price': '10.00'
     }
 ]
 
 const server = setupServer(
-    rest.get('/products', (req, res, ctx) => {
+    rest.get(`${ENDPOINT}/products`, (req, res, ctx) => {
         return res(ctx.json(dummyResponse))
     })
 )
