74d41edd316a4c1688bcc7f8ff6a0ed144f12df6
Mauricio Giacomello <mvgiacomello@gmail.com>
2021-01-23 17:59:09 +0000

    A better looking details page

--- src/common/APIUtils.js
+++ src/common/APIUtils.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'https://demo4690370.mockable.io'
+const ENDPOINT = process.env.ENDPOINT || 'https://demo4690370.mockable.io'
 
 async function retrieveProducts() {
     const response = await fetch(`${ENDPOINT}/products`);
@@ -7,23 +7,9 @@ async function retrieveProducts() {
 }
 
 async function retrieveProduct(id) {
-    // const response = await fetch(`${ENDPOINT}/products/${id}`);
-    // const jsonBody = await response.json();
-    // return jsonBody
-    return {
-        "id": 111,
-        "title": "John Legend Live",
-        "description": "Come and join one of the world's most beloved artists as he performs live and just for you from MelodyVR’s studio in LA.",
-        "artist": "John Legend",
-        "isFree": false,
-        "image1": "https://d17jafawxl91z1.cloudfront.net/MVR_FDC_L0103_App_Hero-1607341456001.jpg",
-        "image2": "https://d17jafawxl91z1.cloudfront.net/MVR_FDC_L0103_App_Square-1607341456001.jpg",
-        "accentColor": "#f52459",
-        "backgroundColor": "#841225",
-        "textColor": "#ffffff",
-        "feature_order": "4",
-        "price": "12.99"
-    }
+    const response = await fetch(`${ENDPOINT}/products/${id}`);
+    const jsonBody = await response.json();
+    return jsonBody
 }
 
 export { retrieveProducts, retrieveProduct }
--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,33 +1,56 @@
 import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { retrieveProduct } from '../../common/APIUtils'
 import Header from '../Header/Header'
 
 const ProductDetails = () => {
-    const location = useLocation()
+    const { productId } = useParams()
     const [details, setDetails] = useState({})
 
     useEffect(() => {
         const getDetails = async () => {
-            const detailsFromServer = await retrieveProduct(location.pathname.split('/').reverse()[0])
+            const detailsFromServer = await retrieveProduct(productId)
             setDetails(detailsFromServer)
         }
         getDetails()
-    }, [location])
+    }, [productId])
 
-    const backButtonStyle = {
-        color: 'white',
-        border: 'solid 1px',
+    const returnButtonStyle = {
+        backgroundColor: details.backgroundColor,
+        borderLeft: 'solid 3px #49FFD0',
         borderRadius: '5px',
-        backgroundCollor: '#49FFD0'
+        padding: '5px',
+        color: details.textColor,
+        marginBottom: '20px',
+    }
+
+    const detailsStyle = {
+        backgroundColor: details.backgroundColor,
+        borderLeft: `solid 3px ${details.accentColor}`,
+        borderRadius: '5px',
+        padding: '20px',
+        color: details.textColor,
+        textAlign: 'left'
+    }
+
+    const imgStyle = {
+        borderRadius: '5px',
+        borderRight: `solid 3px ${details.accentColor}`,
     }
 
     return (
         <div>
+            <div style={returnButtonStyle}>
+                <Link to='/'>Return</Link>
+            </div>
             <Header text={details.title} />
-            <Link to='/' style={backButtonStyle}>Return</Link>
-            <p style={backButtonStyle}>{`${JSON.stringify(details)}`}</p>
+            <div style={detailsStyle}>
+                <img src={details.image1} alt={details.artist} style={imgStyle} />
+                <p>Artist: {details.artist}</p>
+                <p>Description: {details.description}</p>
+                <p>Price: £ {details.isFree ? 'FREE' : (details.price === undefined ? 'Not Confirmed' : details.price)}</p>
+            </div>
         </div>
     )
 }
