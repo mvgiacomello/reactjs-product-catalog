c290382cd63f4c9c61227a76ec201e2f745e4239
Mauricio Giacomello <mvgiacomello@gmail.com>
2021-01-23 19:03:28 +0000

    Adding test for ProductGridItem

---
+++ src/components/ProductGridItem/ProductGridItem.test.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render, screen } from '@testing-library/react';
+import ProductGridItem from './ProductGridItem';
+
+test('renders ProductGridItem with Title', () => {
+    render(<Router><ProductGridItem title='Foo' /></Router>);
+    const element = screen.getByText(/Foo/i);
+    expect(element).toBeInTheDocument();
+});
+
+test('renders ProductGridItem with Price', () => {
+    render(<Router><ProductGridItem price='10' /></Router>);
+    const element = screen.getByText(/£ 10/i);
+    expect(element).toBeInTheDocument();
+});
